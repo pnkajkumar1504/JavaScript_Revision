7ad7faa415728f1a3ef002d3a663280a9e4a6bd6
Pankaj kumar <154460162+pnkajkumar1504@users.noreply.github.com>
2025-12-28 16:52:26 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // Treat All js code as neer version
+//alert(3+3); We Are Using Nodejjs Not Browser
+console.log(3+3);
+console.log("Prem");
+
+let name="Pankaj"
+let age= 22;
+let isLoggedIn=true;
+
+// number => 2 to the power something like 53 
+// bigint => for very big number 
+// string => "something inside "
+//boolean => true or False 
+// null => standalone value
+// undefined => 
+// symbol => For Uniqueness
+
+
+// object
+
+console.log(typeof("Pankaj"));
+console.log(typeof age);
+// interview question 
+console.log(typeof null);  // Null is object deta type
+console.log(typeof undefined);  // undefined is itself a data types  
