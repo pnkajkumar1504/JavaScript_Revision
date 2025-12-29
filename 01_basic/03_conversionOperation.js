ea799adfbf50bd4ac4128fe7aef0ba6786939b40
Pankaj kumar <154460162+pnkajkumar1504@users.noreply.github.com>
2025-12-29 02:06:03 +0000

    Operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -28,10 +28,50 @@
 
 
 
-let someNumber= 58;
-let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber= 58;
+// let stringNumber= String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+/**********************************Opreation********************************/
+
+// let value =3
+// let negValue=-value
+// console.log(negValue);  // -3
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2 To the Power 2
+// console.log(2/2); 
+// console.log(2%2); 
+
+// console.table([2+2 , 2-2, 2*2, 2**2 , 2/2 , 2%2])
+
+
+// let str1="Hello "
+// let str2= "Pankaj"
+// let str3= str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);  //12
+// console.log(1+"2");  // 12
+// console.log("1"+2+2);  //122
+// console.log(1+2+"2"); //32
+
+
+
+//console.log(true);  // true
+//console.log(+true)  // 1
+//console.log(+"");  // 0
+
+let gameCount =100;
+gameCount++;
+console.log(gameCount);
+
+
+
 
 
 
