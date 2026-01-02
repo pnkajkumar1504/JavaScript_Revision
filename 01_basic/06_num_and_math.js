aee26d13a54abb3e8e8a0f15015fef59a7fda8d2
Pankaj kumar <154460162+pankajkumar-code@users.noreply.github.com>
2026-01-02 16:59:14 +0000

    math and num

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,44 @@
+const score=300;
+//console.log(score);
+
+const  balance = new Number(200);
+// console.log(balance);
+
+// console.log(balance.toString()); 
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));  //200.00
+
+const number=1000000
+//console.log(number.toLocalString());
+//console.log(number.toLocaleString()); // US Standard
+//console.log(number.toLocaleString("en-IN"));
+
+/***************************************** MAth ************************************* */
+// console.log(Math);
+// console.log(Math.abs(-3)); // 3
+// console.log(Math.round(3.2)); // 3
+// console.log(Math.round(3.6)); // 4
+// console.log(Math.ceil(2.1)); // 3
+// console.log(Math.floor(2.9)); // 2
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log (Math.floor((Math.random()*(max-min+1)+min)));
+
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
